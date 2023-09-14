d23699e5029f036591a31fccf19a05524673495a
CodeSandbox <support@codesandbox.io>
2023-09-14 12:39:38 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 export function App() {
   return (
-    <div className="font-semibold p-2">
-      <center>Vite ⚡+ React ⚛️ + Tailwindcss 🍃</center>
+    <div className="grid1 font-semibold p-2">
+     <header className="headerNavbar">
+        <h1 className="header1">API<span className="colorWhite">.lab</span></h1>
+        <nav>
+          <button>Home</button>
+          <button>Shop</button>
+          <button>Cart</button>
+          </nav>
+     </header>
+     <div className="main">1</div>
+      <footer className="footer">
+        <a href="https://github.com/botechia-erika" target="_blank">@Botechia-Erika</a>
+      </footer>
     </div>
   );
 
